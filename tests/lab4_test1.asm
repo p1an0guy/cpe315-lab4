4bac9d9287506e5cfb7d224c2ff30bde63a5903b
p1an0guy <123jonahchan@gmail.com>
2026-05-13 17:50:15 -0700

    fix: spacing on test files

--- tests/lab4_test1.asm
+++ tests/lab4_test1.asm
@@ -3,33 +3,33 @@
 # CPI = 1.400   cycles = 42 instructions = 30
 
 # instructions with rd as destination
-    
-    lw $a0, 0($a1)  
-    add $t0, $a0, $a1   # stall
 
-    lw $a0, 0($a1)
-    add $t0, $a1, $a0   # stall
+lw $a0, 0($a1)  
+add $t0, $a0, $a1   # stall
 
-    lw $a0, 0($a1)
-    add $t0, $t0, $t0   # no stall
+lw $a0, 0($a1)
+add $t0, $a1, $a0   # stall
 
-    lw $a0, 0($a1)
-    sub $t0, $a1, $a0   # stall
+lw $a0, 0($a1)
+add $t0, $t0, $t0   # no stall
 
-    lw $a0, 0($a1)
-    sub $t0, $a0, $a1   # stall
+lw $a0, 0($a1)
+sub $t0, $a1, $a0   # stall
 
-    lw $a0, 0($a1)  
-    sub $t0, $t0, $t0   # no stall
+lw $a0, 0($a1)
+sub $t0, $a0, $a1   # stall
 
-    lw $a0, 0($a1)      
-    slt $t0, $a0, $a1   # stall
+lw $a0, 0($a1)  
+sub $t0, $t0, $t0   # no stall
 
-    lw $a0, 0($a1)
-    slt $t0, $a1, $a0   # stall
+lw $a0, 0($a1)      
+slt $t0, $a0, $a1   # stall
 
-    lw $a0, 0($a1)
-    slt $t0, $t0, $t0   # no stall
+lw $a0, 0($a1)
+slt $t0, $a1, $a0   # stall
+
+lw $a0, 0($a1)
+slt $t0, $t0, $t0   # no stall
 
 # instructions with rt as destination
 
@@ -50,6 +50,6 @@ lw $a1, 0($a0)      # stall
 
 lw $0, 0($s1)       # no stall
 add $s2, $s1, $0
-    
+
 
 
